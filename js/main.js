1ca9727a378c15ed85697652f3d1ce88239fd010
MohammedAyman2018 <emidobibo20@gmail.com>
2019-11-01 14:01:58 +0200

    End Basic details

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-window.onload = function (){
-//    Animate();    
-    ScrollOut();
+window.onload = function () {
+  //    Animate();    
+  ScrollOut();
 }
+
+
+let Select = [...document.querySelectorAll('.js-choice')];
+
+Select.forEach(inpt => {
+  new Choices(inpt, {
+    searchEnabled: true,
+    searchChoices: true,
+    noResultsText: 'لا توجد نتيجة',
+    noChoicesText: 'لا يوجد',
+    itemSelectText: 'اضغط لتختار',
+    renderChoiceLimit: 1000,
+  });
+});
