6efa8fa5fff61d9dffeb10c46b582b1ea9c9aad7
MohammedAyman2018 <emidobibo20@gmail.com>
2019-11-03 22:16:34 +0200

    Add upload images buttons

--- js/main.js
+++ js/main.js
@@ -16,3 +16,15 @@ Select.forEach(inpt => {
     renderChoiceLimit: 1000,
   });
 });
+
+var file = [...document.querySelectorAll('[data-get]')] ,
+    preview = [...document.querySelectorAll('[data-preview]')] ;
+
+file.forEach(input => {
+  input.onchange = () => {
+    let me = preview.filter( image =>  image.getAttribute('data-preview') == input.getAttribute('data-get') );
+
+    me[0].src = URL.createObjectURL(input.files[0])
+  };
+});
+
