560f44be97a825d953001c1b873366299af82ced
MohammedAyman2018 <emidobibo20@gmail.com>
2019-12-08 01:23:46 +0200

    fixing image previwer

--- public/js/modules/ImagePreview.js
+++ public/js/modules/ImagePreview.js
@@ -1,12 +1,24 @@
 
 // preview the image 
 var file = [...document.querySelectorAll('[data-get]')],
-  preview = [...document.querySelectorAll('[data-preview]')];
+    preview = [...document.querySelectorAll('[data-preview]')];
 
 file.forEach(input => {
   input.onchange = () => {
-    let me = preview.filter(image => image.getAttribute('data-preview') == input.getAttribute('data-get'));
+    let exactImage = preview.filter(image => image.getAttribute('data-preview') == input.getAttribute('data-get'))[0];
 
-    me[0].src = URL.createObjectURL(input.files[0])
+    input.style.display = "none";
+
+    exactImage.classList.add('z-50');
+    exactImage.setAttribute('src', URL.createObjectURL(input.files[0])); 
+  };
+});
+
+
+preview.forEach(img => {
+  img.onclick = () => {
+    let exactInput = file.filter(inpt => inpt.getAttribute('data-get') == img.getAttribute('data-preview') );
+
+    exactInput[0].click();
   };
 });
